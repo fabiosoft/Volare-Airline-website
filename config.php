5350051dd720350d099b6ac3e6263b74e52b230e
Fabio Nisci <fabionisci@gmail.com>
2015-06-30 15:53:19 +0200

    footer info with author name and page title

--- config.php
+++ config.php
@@ -3,4 +3,5 @@
 	define("DB_USER", 'root');
 	define("DB_PASSWORD", 'root');
 	define("DB_DATABASE", 'volare');
+    define("AUTHOR_NAME", 'Autore');
 ?>
--- footer.php
+++ footer.php
@@ -1,12 +1,5 @@
 <div class="wrapper">
-    <ul id="icons">
-        <li><a href="#" class="normaltip"><img src="images/icon1.jpg" alt=""></a></li>
-        <li><a href="#" class="normaltip"><img src="images/icon2.jpg" alt=""></a></li>
-        <li><a href="#" class="normaltip"><img src="images/icon3.jpg" alt=""></a></li>
-        <li><a href="#" class="normaltip"><img src="images/icon4.jpg" alt=""></a></li>
-        <li><a href="#" class="normaltip"><img src="images/icon5.jpg" alt=""></a></li>
-        <li><a href="#" class="normaltip"><img src="images/icon6.jpg" alt=""></a></li>
-    </ul>
-    <div class="links">Copyright &copy; <a href="#">Domain Name</a> All Rights Reserved<br>
-        Design by <a target="_blank" href="http://www.templatemonster.com/">TemplateMonster.com</a></div>
+    <?php include_once('config.php') ?>
+    <div class="links">Copyright &copy; <a href="#"><?php echo AUTHOR_NAME ?></a> - All Rights Reserved<br>
+        Pagina: <?php echo basename($_SERVER['PHP_SELF']) ?></div>
 </div>
