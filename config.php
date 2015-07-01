4a9bcbd562c9deef4e88d3bda3cd008de8cfe1c3
Fabio Nisci <fabionisci@gmail.com>
2015-07-01 22:52:06 +0200

    fixing of new user and new password as declared in volare.sql database script

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	define("DB_HOST", 'localhost');
-	define("DB_USER", 'root');
-	define("DB_PASSWORD", 'root');
+	define("DB_USER", 'uAdmin');
+	define("DB_PASSWORD", 'SuperPippo!!!');
 	define("DB_DATABASE", 'volare');
     define("AUTHOR_NAME", 'Autore');
 ?>
