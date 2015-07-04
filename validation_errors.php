744d9d26494679a49ebd07a07df456bc834d04dd
Fabio Nisci <fabionisci@gmail.com>
2015-07-04 19:03:53 +0200

    info about how to use validation template

--- validation_errors.php
+++ validation_errors.php
@@ -1,4 +1,11 @@
-<?php // $val_errors must be in page scope ?>
+<?php
+// $val_errors must be in page scope
+// Typical use:
+// <ul class="pad_bot1 list1">
+//      include_once("validation_errors.php")
+// </ul>
+//
+ ?>
 <?php if(isset($val_errors)): ?>
     <?php foreach($val_errors as $error) :?>
         <li><b>Validazione errata: </b><?php echo $error ?></li>
