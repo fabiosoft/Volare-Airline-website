8465138dfe9681b103c2bfc48f1ebce8a08123b1
Fabio Nisci <fabionisci@gmail.com>
2015-07-06 15:01:01 +0200

    logout link in top right navigation menu

--- top_nav.php
+++ top_nav.php
@@ -1,12 +1,23 @@
 <ul>
     <li><a href="home.php" class="nav1">Home</a></li>
-    <li><a href="#" class="nav2">
+    <li><a href="#" class="nav3">
             <b>
                 <?php
                 echo $current_user->getUserName();
                 if ($current_user->isLoggedIn())
-                    echo "<br/>" . "&euro;" . $current_user->getMoney();
+                    echo "&euro;" . $current_user->getMoney();
                 ?>
+                <?php if ($current_user->isLoggedIn()) : ?>
+
+                    <form id="form_1" name="login" method="post" action="home.php">
+                    <div class="wrapper">
+                        <span class="right relative">
+                            <input type="hidden" name="logout">
+                            <a href="home.php" onclick="document.forms[0].submit();return false;">Logout</a>
+                        </span>
+                    </div>
+                </form>
+                <?php endif; ?>
             </b>
         </a>
     </li>
